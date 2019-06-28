76ba451764ae441f0baac7d95d69b2d23a56e5df
Asim Zaidi <asimzaidih@gmail.com>
2019-06-27 21:19:03 -0700

    working on maxProfit

--- Meeting-Problems/meeting_3.py
+++ Meeting-Problems/meeting_3.py
@@ -23,20 +23,45 @@ def find(prices):
 # print(find([100,2,5,200,50,1,400])) # 399 ✅
 
 
-
 # Problem 1:
 
 def maxProfit(prices):
     max_profit = 0
-    min_val = prices[0]
-    for val in prices:
-        if val < min_val:
-            min_val = val
+    lowest_val = prices[0]
+    potential_options = []
+    # Go through each value in prices
+    for idx, val in enumerate(prices):
+        print('Iteration:', idx)
+        print('lowest_val:', lowest_val, 'val:', val)
+        # if the val is less than lowest value, set lowest_val to that val and continue
+        if val <= lowest_val:
+            print('Set lowest value')
+            lowest_val = val
             continue
-        curr_val = val - min_val
-        if val > min_val:
-            max_profit += curr_val
+        # if we found a potential profit
+        if val > lowest_val:
+            print('found a potential profit, added to potential options.')
+            # add the value to our list of potential values
+            potential_options.append(val)
+            # Don't do this yet!: max_profit += val - lowest_val 
+            # If we haven't reached the end
+            try:
+                # set the lowest_val to the next val if its less than or equal to where we're at.
+                if prices[idx+1] <= val:
+                        lowest_val = prices[idx+1]
+                # If not, we keep our lowest value where its at.
+            except IndexError:
+                print(potential_options)
+                max_profit = max(potential_options) - lowest_val
+                return max_profit
 
     return max_profit
 
-print(maxProfit([20,10,30,0,20])) # 40
+# print(maxProfit([7,1,5,3,6,4])) # 7
+# print(maxProfit([7,1,5])) # 4
+print(maxProfit([1,2,3,4,5])) # 4
+# potential_options.append(val)
+# if val <= prices[idx+1]:
+#     min_val = prices[idx+1]
+#     max_profit += max(potential_options)
+#     potential_options = []
